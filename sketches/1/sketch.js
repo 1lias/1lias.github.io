0f8eda57ca42cdeaa40e677578ce3323329c9a5e
1lias <iliaspa7@gmail.com>
2020-06-20 19:14:03 +0200

    Update to sketch.js

--- sketches/1/sketch.js
+++ sketches/1/sketch.js
@@ -1,38 +1,36 @@
-let sketch = function(p) {
+let rings = 60;  
 
-  let rings = 2;  
 
-  p.setup = function(){
-    p.createCanvas(840, 840);
-    p.strokeWeight(2);
-    p.stroke(17,17,17);
-    p.noFill();
-  }
 
-  p.draw = function() {
-    p.clear();
-    p.background(255,240,250)
-    p.translate(50,50)
-    for(let i = 0; i < 8; i++){
-      for(let j=0; j < 8; j++){
-        display(i*100,j*100);
-      }
-    }  
-  }
-
-  function display(x, y) {
-    for(let i = 0; i < rings; i ++){
-      p.translate(20, 0);
-      p.circle(x+10, y+10, 20 + i*20);
-      p.translate(0, 20);
-      p.circle(x+10, y+10, 20 + i*20);
-      p.translate(-20, 0);
-      p.circle(x+10, y+10, 20 + i*20);
-      p.translate(0, -20);
-      p.circle(x+10, y+10, 20 + i*20);
-    }
-  }
-
+function setup() {
+  createCanvas(1600, 1600);
+  strokeWeight(2);
+  stroke(17,17,17);
+  noFill()
 }
 
-new p5(sketch);
+function draw() {
+  clear();
+  background(255,240,250);
+  translate(width / 2, height / 2);
+  displayCirc();
+  displayRect();
+}
+
+
+function displayCirc() {
+  for(let i = 0; i < rings; i ++){
+    noFill();
+    stroke(17,17,17);
+    circle(0, 0, 20 + i*20);    
+  }
+}
+
+function displayRect() {
+  for(let i = 0; i < rings*10; i ++);
+    fill(0)
+    noStroke()
+    rect(40, -20, 520, 40);
+}
+
+save("processing1.png");
